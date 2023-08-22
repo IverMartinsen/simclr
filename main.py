b30a0e7ba1b029ab79807680d6c41709bdd8c590
Iver Martinsen <iver.martinsen@outlook.com>
2023-08-22 16:32:33 +0200

    moved simclr file to folder

---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+import sys
+
+sys.path.append(os.getcwd())
+
+import argparse
+import wandb
+import tensorflow as tf
+from datetime import datetime
+from scampi_unsupervised.frameworks.simclr.simclr import ContrastiveModel, get_augmenter, get_encoder, get_projection_head, Augmenter
+from scampi_unsupervised.dataloader import get_tfrecord_dataset, get_tfrecord_benchmark_dataset, get_dataset_from_hdf5_old
+from scampi_unsupervised.tf_utils import LogisticRegressionCallback
+from scampi_evaluation.prepare_labelled_data import get_numpy_dataset
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", type=int, default=100)
+    parser.add_argument("--batch_size", type=int, default=128)
+    parser.add_argument("--buffer_size", type=int, default=100000)
+    parser.add_argument("--dataset_size", type=int, default=200000)
+    parser.add_argument("--input_shape", type=int, nargs="+", default=[96, 96, 3])
+    parser.add_argument("--temperature", type=float, default=0.1)
+    
+    #dataset = get_tfrecord_dataset(batch_size=parser.parse_args().batch_size, buffer_size=parser.parse_args().buffer_size)
+    dataset = get_tfrecord_benchmark_dataset(batch_size=parser.parse_args().batch_size, buffer_size=parser.parse_args().buffer_size, dataset_size=parser.parse_args().dataset_size)
+    #dataset = get_dataset_from_hdf5_old(image_shape=parser.parse_args().input_shape, batch_size=parser.parse_args().batch_size, dataset_size=parser.parse_args().dataset_size)
+    dataset_labelled = get_numpy_dataset(num_classes=10, splits=[0.8, 0.2], seed=1234)
+    
+    timestr = datetime.now().strftime("%Y%m%d_%H%M%S")
+    
+    train_id = "simclr_" + timestr
+    
+    wandb.init(project="scampi", name=train_id, config=vars(parser.parse_args()))
+    
+    model = ContrastiveModel(
+        augmenter = get_augmenter(input_shape=parser.parse_args().input_shape, min_area=0.25, brightness=0.6, jitter=0.2),
+        encoder = tf.keras.applications.Xception(include_top=False, pooling="avg", input_shape=parser.parse_args().input_shape, weights=None), 
+        projection_head = get_projection_head(width=128, input_shape=(2048, )),
+        temperature = parser.parse_args().temperature,
+    )
+    
+    optimizer = tf.keras.optimizers.Adam()
+    
+    model.compile(optimizer=optimizer, probe_optimizer=None)
+
+    callbacks = []
+    callbacks.append(wandb.keras.WandbCallback(save_model=False))
+    callbacks.append(LogisticRegressionCallback(dataset_labelled, log_freq=5))
+    
+    history = model.fit(
+        dataset, 
+        epochs=parser.parse_args().epochs,
+        steps_per_epoch=None,
+        validation_data=None,
+        callbacks=callbacks,
+        )
+    
+    model.encoder.save_weights("./scampi_unsupervised/frameworks/simclr/" + train_id + "_encoder.h5")
---
+++ simclr.py
@@ -0,0 +1,263 @@
+# The SimCLR class implementation is mostly copied from the tutorial in https://keras.io/examples/vision/semisupervised_simclr/
+import tensorflow as tf
+import math
+from scampi_unsupervised.data_augmentation import (
+    random_crop,
+    random_jitter,
+    random_color_drop,
+)
+
+rng = tf.random.Generator.from_seed(42)
+
+
+def get_projection_head(width, input_shape):
+    return tf.keras.Sequential(
+        [
+            tf.keras.Input(shape=input_shape),
+            tf.keras.layers.Dense(width, activation="relu"),
+            tf.keras.layers.Dense(width),
+            ],
+            name="projection_head",
+        )
+
+
+def get_encoder(width, input_shape):
+    return tf.keras.Sequential(
+        [
+            tf.keras.Input(shape=input_shape),
+            tf.keras.layers.Conv2D(width, kernel_size=3, strides=2, activation="relu"),
+            tf.keras.layers.Conv2D(width, kernel_size=3, strides=2, activation="relu"),
+            tf.keras.layers.Conv2D(width, kernel_size=3, strides=2, activation="relu"),
+            tf.keras.layers.Conv2D(width, kernel_size=3, strides=2, activation="relu"),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(width, activation="relu"),
+        ],
+        name="encoder",
+    )
+
+
+class Augmenter(tf.keras.layers.Layer):
+    def __init__(self, input_size, batch_size, **kwargs):
+        super().__init__(**kwargs)
+        self.input_size = input_size
+        self.batch_size = batch_size
+
+    def call(self, x, training=True):
+        # rescale
+        x = tf.cast(x, tf.float32) / 255.0
+        if training:
+            x = random_crop(
+                x,
+                min_area=0.25,
+                height=self.input_size[0],
+                width=self.input_size[1],
+                batch_size=self.batch_size,
+            )
+            x = tf.image.random_flip_left_right(x)
+            x = random_jitter(x, p=0.8, s=1.0)
+            x = random_color_drop(x, p=0.2)
+        return x
+
+
+def get_augmenter(input_shape, min_area, brightness, jitter):
+    zoom_factor = 1.0 - math.sqrt(min_area)
+    return tf.keras.Sequential(
+        [
+            tf.keras.Input(shape=input_shape),
+            tf.keras.layers.Rescaling(1 / 255),
+            tf.keras.layers.RandomFlip("horizontal"),
+            tf.keras.layers.RandomTranslation(zoom_factor / 2, zoom_factor / 2),
+            tf.keras.layers.RandomZoom((-zoom_factor, 0.0), (-zoom_factor, 0.0)),
+            RandomColorAffine(brightness, jitter),
+        ]
+    )
+
+
+
+class RandomColorAffine(tf.keras.layers.Layer):
+    """
+    Random color affine transformations as in Keras tutorial
+    """
+
+    def __init__(self, brightness=0, jitter=0, **kwargs):
+        super().__init__(**kwargs)
+
+        self.brightness = brightness
+        self.jitter = jitter
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"brightness": self.brightness, "jitter": self.jitter})
+        return config
+
+    def call(self, images, training=True):
+        if training:
+            batch_size = tf.shape(images)[0]
+            # Same for all colors
+            brightness_scales = 1 + rng.uniform(
+                (batch_size, 1, 1, 1), minval=-self.brightness, maxval=self.brightness
+            )
+            # Different for all colors
+            jitter_matrices = rng.uniform(
+                (batch_size, 1, 3, 3), minval=-self.jitter, maxval=self.jitter
+            )
+            # Combine brightness and jitter
+            color_transforms = (
+                tf.eye(3, batch_shape=[batch_size, 1]) * brightness_scales
+                + jitter_matrices
+            )
+            # cast to input type
+            color_transforms = tf.cast(color_transforms, images.dtype)
+            # Apply all color transformations
+            images = tf.clip_by_value(tf.matmul(images, color_transforms), 0, 1)
+        return images
+
+
+class ContrastiveModel(tf.keras.Model):
+    """TensorFlow SimCLR model for contrastive learning."""
+
+    def __init__(self, augmenter, encoder, projection_head, temperature=0.1, **kwargs):
+        super().__init__(name="simclr", **kwargs)
+        # === Hyperparameters ===
+        self.temperature = temperature
+        # === Architecture ===
+        self.augmenter = augmenter
+        self.encoder = encoder
+        self.projection_head = projection_head
+        # === Linear probe ===
+        self.linear_probe = None
+        self.labeled_dataset = None
+
+    def compile(self, optimizer, probe_optimizer, **kwargs):
+        """
+        Initialize the contrastive and probe optimizers and losses.
+        Note that self.contrastive_loss is not here, but is defined as a method below.
+        """
+        super().compile(**kwargs)
+
+        self.contrastive_optimizer = optimizer
+        self.probe_optimizer = probe_optimizer
+
+        self.probe_loss = tf.keras.losses.SparseCategoricalCrossentropy(
+            from_logits=True
+        )
+
+        self.contrastive_loss_tracker = tf.keras.metrics.Mean(name="contrastive_loss")
+        self.contrastive_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
+            name="contrastive_acc"
+        )
+        self.probe_loss_tracker = tf.keras.metrics.Mean(name="prediction_loss")
+        self.probe_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
+            name="prediction_acc"
+        )
+
+    @property
+    def metrics(self):
+        return super().metrics + [
+            self.contrastive_loss_tracker,
+            self.contrastive_accuracy,
+            self.probe_loss_tracker,
+            self.probe_accuracy,
+        ]
+
+    def contrastive_loss(self, projections_1, projections_2):
+        """
+        Compute the contrastive loss - NT-Xent loss (normalized temperature-scaled cross entropy) - for two sets of projections.
+        Possibly equal to the InfoNCE loss (information noise-contrastive estimation) described elsewhere.
+        """
+        # Cosine similarity: the dot product of the l2-normalized feature vectors
+        projections_1 = tf.math.l2_normalize(projections_1, axis=1)
+        projections_2 = tf.math.l2_normalize(projections_2, axis=1)
+        similarities = (
+            tf.matmul(projections_1, projections_2, transpose_b=True) / self.temperature
+        )
+
+        # The similarity between the representations of two augmented views of the
+        # same image should be higher than their similarity with other views
+        batch_size = tf.shape(projections_1)[0]
+        contrastive_labels = tf.range(batch_size)
+        self.contrastive_accuracy.update_state(contrastive_labels, similarities)
+        self.contrastive_accuracy.update_state(
+            contrastive_labels, tf.transpose(similarities)
+        )
+
+        # The temperature-scaled similarities are used as logits for cross-entropy
+        # a symmetrized version of the loss is used here
+        loss_1_2 = tf.keras.losses.sparse_categorical_crossentropy(
+            contrastive_labels, similarities, from_logits=True
+        )
+        loss_2_1 = tf.keras.losses.sparse_categorical_crossentropy(
+            contrastive_labels, tf.transpose(similarities), from_logits=True
+        )
+        return (loss_1_2 + loss_2_1) / 2
+
+    def train_step(self, images):
+        """
+        In the Keras tutorial, both labeled and unlabeled images are concatenated for contrastive learning.
+        Here, we only use the unlabeled images for contrastive learning.
+        """
+        augmented_images_1 = self.augmenter(images, training=True)
+        augmented_images_2 = self.augmenter(images, training=True)
+
+        with tf.GradientTape() as tape:
+            # Each augmented image is passed through the encoder
+            features_1 = self.encoder(augmented_images_1, training=True)
+            features_2 = self.encoder(augmented_images_2, training=True)
+            # The representations are passed through a projection mlp
+            projections_1 = self.projection_head(features_1, training=True)
+            projections_2 = self.projection_head(features_2, training=True)
+            # The contrastive loss is computed on the projections
+            contrastive_loss = self.contrastive_loss(projections_1, projections_2)
+
+        gradients = tape.gradient(
+            contrastive_loss,
+            self.encoder.trainable_weights + self.projection_head.trainable_weights,
+        )
+
+        self.contrastive_optimizer.apply_gradients(
+            zip(
+                gradients,
+                self.encoder.trainable_weights + self.projection_head.trainable_weights,
+            )
+        )
+        self.contrastive_loss_tracker.update_state(contrastive_loss)
+
+        #########################################
+        ### The next part is the linear probe ###
+        #########################################
+        if self.linear_probe is not None:
+            # Labels are only used in evalutation for an on-the-fly logistic regression
+            images, labels = next(iter(self.labeled_dataset))
+            images = images / 255.0
+            # Sample weights are used for class balancing
+            sample_weight = self.class_weights.lookup(labels)
+            with tf.GradientTape() as tape:
+                # the encoder is used in inference mode here to avoid regularization
+                # and updating the batch normalization paramers if they are used
+                features = self.encoder(images, training=False)
+                class_logits = self.linear_probe(features, training=True)
+                probe_loss = self.probe_loss(
+                    labels, class_logits, sample_weight=sample_weight
+                )
+            gradients = tape.gradient(probe_loss, self.linear_probe.trainable_weights)
+            self.probe_optimizer.apply_gradients(
+                zip(gradients, self.linear_probe.trainable_weights)
+            )
+            self.probe_loss_tracker.update_state(probe_loss)
+            self.probe_accuracy.update_state(labels, class_logits)
+
+        return {m.name: m.result() for m in self.metrics}
+
+    def test_step(self, data):
+        labeled_images, labels = data
+
+        # For testing the components are used with a training=False flag
+        preprocessed_images = labeled_images / 255.0
+        features = self.encoder(preprocessed_images, training=False)
+        class_logits = self.linear_probe(features, training=False)
+        probe_loss = self.probe_loss(labels, class_logits)
+        self.probe_loss_tracker.update_state(probe_loss)
+        self.probe_accuracy.update_state(labels, class_logits)
+
+        # Only the probe metrics are logged at test time
+        return {m.name: m.result() for m in self.metrics[2:]}
