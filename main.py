1172a9c2b02d9d5ff6058a5c0fc21f001da80b04
Iver Martinsen <iver.martinsen@outlook.com>
2023-12-14 16:17:38 +0100

    use tfrecords ds instead, move rescaling to preprocessing

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import sys
 sys.path.append(os.getcwd())
 
 import argparse
+import glob
 import wandb
 import tensorflow as tf
 from datetime import datetime
 from simclr import ContrastiveModel, get_augmenter, get_encoder, get_projection_head
-#from scampi_unsupervised.dataloader import get_tfrecord_dataset, get_tfrecord_benchmark_dataset, get_dataset_from_hdf5_old
 #from scampi_unsupervised.tf_utils import LogisticRegressionCallback
 #from scampi_evaluation.prepare_labelled_data import get_numpy_dataset
 
@@ -22,37 +22,54 @@ if __name__ == "__main__":
     parser.add_argument("--temperature", type=float, default=0.1)
     parser.add_argument("--loss_implementation", type=str, default="simple")
     parser.add_argument("--path_to_files", type=str, default="./data/NO 6407-6-5/100K_BENCHMARK_224x224/images/")
+    args = parser.parse_args()
     
-    dataset = tf.data.Dataset.list_files(parser.parse_args().path_to_files + "*.png")
-    dataset = dataset.shuffle(parser.parse_args().buffer_size)
-    dataset = dataset.map(lambda x: tf.io.read_file(x))
-    dataset = dataset.map(lambda x: tf.image.decode_png(x, channels=3))
-    dataset = dataset.map(lambda x: tf.image.resize(x, parser.parse_args().input_shape[:2]))
-    dataset = dataset.batch(parser.parse_args().batch_size, drop_remainder=True)
+    for arg in vars(args):
+        print(arg, getattr(args, arg))
+    
+    #dataset = tf.data.Dataset.list_files(args.path_to_files + "*.png")
+    #dataset = dataset.shuffle(args.buffer_size)
+    #dataset = dataset.map(lambda x: tf.io.read_file(x))
+    #dataset = dataset.map(lambda x: tf.image.decode_png(x, channels=3))
+    #dataset = dataset.map(lambda x: tf.image.resize(x, args.input_shape[:2]))
+    #dataset = dataset.batch(args.batch_size, drop_remainder=True)
+    #dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+    
+    def _tfrecord_map_function(x):
+        """Parse a single image from a tfrecord file."""
+        # Dict with key 'image' and value of type string
+        x = tf.io.parse_single_example(x, {"image": tf.io.FixedLenFeature([], tf.string)})
+        # Tensor of type uint8
+        x = tf.io.parse_tensor(x["image"], out_type=tf.uint8)
+        x = tf.ensure_shape(x, [224, 224, 3])
+        return x
+    
+    dataset = tf.data.TFRecordDataset(glob.glob(args.path_to_files + "*.tfrecords"))
+    dataset = dataset.shuffle(args.buffer_size)
+    dataset = dataset.map(_tfrecord_map_function)
+    dataset = dataset.map(lambda x: tf.image.resize(x, args.input_shape[:2]))
+    dataset = dataset.map(lambda x: x / 255.0)
+    dataset = dataset.batch(args.batch_size, drop_remainder=True)
     dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
-        
-    #dataset = get_tfrecord_dataset(batch_size=parser.parse_args().batch_size, buffer_size=parser.parse_args().buffer_size)
-    #dataset = get_tfrecord_benchmark_dataset(batch_size=parser.parse_args().batch_size, buffer_size=parser.parse_args().buffer_size, dataset_size=parser.parse_args().dataset_size)
-    #dataset = get_dataset_from_hdf5_old(image_shape=parser.parse_args().input_shape, batch_size=parser.parse_args().batch_size, dataset_size=parser.parse_args().dataset_size)
-    #dataset_labelled = get_numpy_dataset(num_classes=10, splits=[0.8, 0.2], seed=1234, img_shape=parser.parse_args().input_shape)
+    #dataset_labelled = get_numpy_dataset(num_classes=10, splits=[0.8, 0.2], seed=1234, img_shape=args.input_shape)
     
     timestr = datetime.now().strftime("%Y%m%d_%H%M%S")
     
     train_id = "simclr_" + timestr
     
-    wandb.init(project="scampi", name=train_id, config=vars(parser.parse_args()))
+    wandb.init(project="scampi", name=train_id, config=vars(args))
     
     model = ContrastiveModel(
-        augmenter = get_augmenter(input_shape=parser.parse_args().input_shape, min_area=0.25, brightness=0.6, jitter=0.2),
-        encoder = tf.keras.applications.Xception(include_top=False, pooling="avg", input_shape=parser.parse_args().input_shape, weights=None), 
+        augmenter = get_augmenter(input_shape=args.input_shape, min_area=0.25, brightness=0.6, jitter=0.2),
+        encoder = tf.keras.applications.Xception(include_top=False, pooling="avg", input_shape=args.input_shape, weights=None), 
         projection_head = get_projection_head(width=128, input_shape=(2048, )),
-        temperature = parser.parse_args().temperature,
-        loss_implementation = parser.parse_args().loss_implementation,
+        temperature = args.temperature,
+        loss_implementation = args.loss_implementation,
     )
     
     schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
         initial_learning_rate=1e-3,
-        decay_steps=parser.parse_args().dataset_size // parser.parse_args().batch_size,
+        decay_steps=args.dataset_size // args.batch_size,
         decay_rate=0.05,
         staircase=True)
 
@@ -69,7 +86,7 @@ if __name__ == "__main__":
     
     history = model.fit(
         dataset, 
-        epochs=parser.parse_args().epochs,
+        epochs=args.epochs,
         steps_per_epoch=None,
         validation_data=None,
         callbacks=callbacks,
