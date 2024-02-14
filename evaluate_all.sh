050becc8232faad7b403a7febea14aad332955f9
Iver Martinsen <iver.martinsen@outlook.com>
2024-02-14 16:39:38 +0100

    change

--- evaluate_all.sh
+++ evaluate_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Evaluate all the models
-PATH_TO_MODELS='/Users/ima029/Desktop/SCAMPI/Repository/scampi_unsupervised/frameworks/simclr/trained models/simclr_20240201_141701'
+PATH_TO_MODELS='./trained models/simclr_20240207_102210'
 
 for model in $(ls "$PATH_TO_MODELS"); do
     # check for h5 files
@@ -12,7 +12,7 @@ for model in $(ls "$PATH_TO_MODELS"); do
     destination=$PATH_TO_MODELS/$(echo $model | cut -d'.' -f1)"_npd10balanced"
     python linear_evaluation.py \
         --pretrained_weights "$PATH_TO_MODELS/$model" \
-        --path_to_imagefolder "/Users/ima029/SCAMPI DATA/labelled crops (NPD)/genera_level/imagefolder10classes_balanced" \
+        --path_to_imagefolder "/Users/ima029/Desktop/SCAMPI/Repository/data/labelled crops (NPD)/genera_level/imagefolder10classes_balanced" \
         --destination "$destination"
 done
 ```
